9a8d4ca48fe8fd94ba60316dd8b2450fbf0c5b69
Subha <samanta.suba@gmail.com>
2016-11-28 00:32:59 -0500

    problem 1 complete

--- problem_1/app.R
+++ problem_1/app.R
@@ -7,7 +7,7 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       sliderInput("yearInput", "Year", min = 1880, max = 2014, value = c(1900, 2000), sep=""),
-      textInput("nameInput", "Name"),
+      textInput("nameInput", "Name", value = "Leslie"),
       radioButtons("sexID", "Sex",  choices = c("Female only", "Male only", "Both"), selected = "Both")),
     mainPanel(
       plotOutput("main_plot"),
@@ -20,9 +20,9 @@ ui <- fluidPage(
 server <- function(input, output, session) {
   reduced_df <- reactive({
     sex_vec <- switch(input$sexID,
-                      `Female only` = "F",
-                      `Male only` = "M",
-                      Both = c("F", "M")
+                      `Female only` = c("F","1"),
+                      `Male only` = c("M","1"),
+                      Both = c("F", "M","2")
     )
     babynames$year <- as.integer(babynames$year)
     filter(
@@ -31,20 +31,25 @@ server <- function(input, output, session) {
       year >= input$yearInput[1] & year <= input$yearInput[2], 
       sex %in% sex_vec 
     )
-  })
+  })  
   output$main_plot <- renderPlot({
     ggplot(data = reduced_df(), 
            aes(year, n, colour = sex)) + 
       geom_line() + ggtitle(input$nameInput) + scale_color_manual(values = c("M" = "blue", "F" = "red"))
   })
-  output$results <- renderTable({ 
-    select(reduced_df(), -prop)
-    
-    ## wide format
+  output$results <- renderTable({
+    if(input$sexID == "Both"){
+      spread(select(reduced_df(),-prop), key = sex, value = n, fill = "0")
+    } else{
+    select(reduced_df(),-prop)}
   })
 }
 
-
-
 shinyApp(ui = ui, server = server)
+
+
+
+  
+  
+  
 
