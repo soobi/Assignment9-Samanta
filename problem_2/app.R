879153872394c13c5ce53dba8c3e112beaff9baa
Subhasree Samanta <samanta.suba@gmail.com>
2016-11-27 20:09:36 -0500

    problem 2 commit

--- problem_2/app.R
+++ problem_2/app.R
@@ -3,16 +3,17 @@ library(gapminder)
 library(tidyverse)
 
 ui <- fluidPage(
+  titlePanel("Logged GDP vs Life Expectancy"),
+  
   sidebarLayout(
     sidebarPanel(
-      sliderInput("yearInput", "Year", min = 1952, max = 2007, value = c(1975, 2000), sep=""),
+      sliderInput("yearInput", "Year", min = 1952, max = 2007, value = c(1952, 2007), sep=""),
       selectInput("countryInput","Country:", choices = gapminder$country)),
       
     mainPanel(
       plotOutput("main_plot")
     )
-  ),
-  titlePanel("Logged GDP vs Life Expectancy")
+  )
 )
 
 server <- function(input, output) {
@@ -23,9 +24,13 @@ server <- function(input, output) {
       year >= input$yearInput[1] & year <= input$yearInput[2]
     )
   })
+  
   output$main_plot <- renderPlot({
     ggplot(data = reduced_df(), 
-           aes(log10(gdpPercap), lifeExp)) + geom_point() + geom_smooth()
+           aes(log10(gdpPercap), lifeExp, color =year)) + 
+      geom_point() + geom_smooth() +
+      theme(plot.title = element_text(size = 15,face = "bold")) +
+      labs(x = "Logged per capita GDP", y = "Life Expectancy", title = input$countryInput) 
   })
 }
 
